c1e12d636113d12a2fd55f3f3a82f9bfc20c84c6
julian <julian.bender@uni-wuerzburg.de>
2026-03-12 14:34:07 +0100

    Next try on black formatting

--- autoprot/analysis/qc.py
+++ autoprot/analysis/qc.py
@@ -242,10 +242,8 @@ def missed_cleavages(df_evidence, enzyme="Trypsin/P", save=True, ax=None, title=
     today = date.today().isoformat()
 
     if "Experiment" not in df_evidence.columns.tolist():
-        print(
-            "Warning: Column [Experiment] either not unique or missing,\n\
-              column [Raw file] used"
-        )
+        print("Warning: Column [Experiment] either not unique or missing,\n\
+              column [Raw file] used")
         experiments = list(set((df_evidence["Raw file"])))
     else:
         experiments = list(set((df_evidence["Experiment"])))
@@ -253,10 +251,8 @@ def missed_cleavages(df_evidence, enzyme="Trypsin/P", save=True, ax=None, title=
     rawfiles = list(set((df_evidence["Raw file"])))
     if len(experiments) != len(rawfiles):
         experiments = rawfiles
-        print(
-            "Warning: Column [Experiment] either not unique or missing,\n\
-              column [Raw file] used"
-        )
+        print("Warning: Column [Experiment] either not unique or missing,\n\
+              column [Raw file] used")
 
     # calculate miss cleavage for each raw file in df_evidence
     df_missed_cleavage_summary = pd.DataFrame()
@@ -613,10 +609,8 @@ def dimethyl_labeling_efficieny(df_evidence, label, save=True) -> pd.DataFrame:
         experiments = list((df_evidence["Experiment"].unique()))
     else:
         experiments = list((df_evidence["Raw file"].unique()))
-        print(
-            "Warning: Column [Experiment] either not unique or missing,\n\
-              column [Raw file] used"
-        )
+        print("Warning: Column [Experiment] either not unique or missing,\n\
+              column [Raw file] used")
 
     df_labeling_eff = pd.DataFrame()
 
